eacd22e41607fc68f0031deebd1fa2b83e404a53
izij <isabel.king@my.jcu.edu.au>
2022-09-25 13:43:30 +1000

    Refactor determine_result and add random score

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,9 +1,25 @@
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+import random
+
+
+def main():
+    score = float(input("Enter score: "))
+    result = determine_result(score)
+    print(result)
+    random_score = random.randint(0, 100)
+    random_result = determine_result(random_score)
+    print(random_score)
+    print(random_result)
+
+
+def determine_result(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
