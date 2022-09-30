24b3cebff0b198e59ce2d6d24bc91ba4b3b54094
izij <isabel.king@my.jcu.edu.au>
2022-09-30 12:33:10 +1000

    Add tasks

---
+++ prac_03/randoms.py
@@ -0,0 +1,8 @@
+# Line 1: smallest number is 5, largest number is 20
+# Line 2: Smallest is 3, largest is 9, going up by 2 each time, so could not produce a 4
+# Line 3: smallest is 2.5, largest is 5.5
+
+import random
+
+print(random.uniform(1, 100))
+
