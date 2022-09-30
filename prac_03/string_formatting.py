e9b4e5227cbeaee32b3e9c19ebf7ab2892ec8d37
izij <isabel.king@my.jcu.edu.au>
2022-09-30 11:56:14 +1000

    Complete TODOs

--- prac_03/string_formatting.py
+++ prac_03/string_formatting.py
@@ -26,6 +26,7 @@ for i, number in enumerate(numbers, 1):
 
 # TODO: Use f-string formatting to produce the output:
 # 1922 Gibson L-5 CES for about $16,035!
+print(f"{year} {name} for about ${cost:,.2f}!")
 
 # TODO: Using a for loop with the range function and string formatting,
 # produce the following right-aligned output (DO NOT use a list):
@@ -33,3 +34,6 @@ for i, number in enumerate(numbers, 1):
 #  50
 # 100
 # 150
+
+for i in range(0, 151, 50):
+    print(f"{i:3}")
