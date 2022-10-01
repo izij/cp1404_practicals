a6fbf643a517278ffd49e57ef825136e9f0cddc6
izij <isabel.king@my.jcu.edu.au>
2022-10-01 17:27:02 +1000

    Add special characters check

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -5,7 +5,7 @@ Password checker "skeleton" code to help you get started
 
 MIN_LENGTH = 2
 MAX_LENGTH = 6
-SPECIAL_CHARS_REQUIRED = False
+SPECIAL_CHARS_REQUIRED = True
 SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
 
 
@@ -20,23 +20,23 @@ def main():
     if SPECIAL_CHARS_REQUIRED:
         print("\tand 1 or more special characters: ", SPECIAL_CHARACTERS)
     password = input("> ")
-    while not is_valid_password(password, MAX_LENGTH, MIN_LENGTH):
+    while not is_valid_password(password):
         print("Invalid password!")
         password = input("> ")
     print("Your {}-character password is valid: {}".format(len(password),
                                                            password))
 
 
-def is_valid_password(password, max_length, min_length):
+def is_valid_password(password):
     """Determine if the provided password is valid."""
-    if len(password) < min_length or len(password) > max_length:
+    if len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
         return False
 
     count_lower = 0
     count_upper = 0
     count_digit = 0
     count_special = 0
-    # TODO: count each kind of character (use str methods like isdigit)
+
     for character in password:
         if character.islower():
             count_lower += 1
@@ -44,10 +44,14 @@ def is_valid_password(password, max_length, min_length):
             count_upper += 1
         elif character.isdigit():
             count_digit += 1
+        elif character in SPECIAL_CHARACTERS:
+            count_special += 1
 
-    # TODO: if any of the 'normal' counts are zero, return False
     if count_lower == 0 or count_upper == 0 or count_digit == 0:
         return False
+    if SPECIAL_CHARS_REQUIRED and count_special == 0:
+        return False
+
 
     # TODO: if special characters are required, then check the count of those
     # and return False if it's zero
