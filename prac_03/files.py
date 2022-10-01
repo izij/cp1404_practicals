aa0ec34bd8887fc36a1c954bc4162e4f61796b15
izij <isabel.king@my.jcu.edu.au>
2022-10-01 16:33:21 +1000

    Check solutions and adjust

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,29 +1,27 @@
 # Question 1
 name = input("Name? ")
-
 out_file = open("name.txt", 'w')
 print(name, file=out_file)
 out_file.close()
 
 # Question 2
 in_file = open("name.txt", 'r')
-name = in_file.read()
+name = in_file.read().strip()
 in_file.close()
-name = name.strip()
 print(f"Your name is {name}")
 
 # Question 3
 in_file = open("numbers.txt", 'r')
-line_1 = in_file.readline()
-line_2 = in_file.readline()
+line_1 = int(in_file.readline())
+line_2 = int(in_file.readline())
 in_file.close()
-result = int(line_1) + int(line_2.strip())
+result = line_1 + line_2
 print(result)
 
 # Question 4
 total = 0
 in_file = open("numbers.txt", 'r')
 for line in in_file:
-    number = int(line.strip())
+    number = int(line)
     total = total + number
 print(total)
