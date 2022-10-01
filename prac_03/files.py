229277e07339793ca328041f393462796f01fd9c
izij <isabel.king@my.jcu.edu.au>
2022-10-01 16:06:14 +1000

    Complete question 2

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,5 +1,13 @@
+# Question 1
 name = input("Name? ")
 
 out_file = open("name.txt", 'w')
 print(name, file=out_file)
 out_file.close()
+
+# Question 2
+in_file = open("name.txt", 'r')
+name = in_file.read()
+in_file.close()
+name = name.strip()
+print(f"Your name is {name}")
