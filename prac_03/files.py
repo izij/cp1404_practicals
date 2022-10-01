e411219c93e45432e5338a16b5402e895c1d8ad8
izij <isabel.king@my.jcu.edu.au>
2022-10-01 16:26:57 +1000

    Complete question 3

--- prac_03/files.py
+++ prac_03/files.py
@@ -11,3 +11,12 @@ name = in_file.read()
 in_file.close()
 name = name.strip()
 print(f"Your name is {name}")
+
+# Question 3
+in_file = open("numbers.txt", 'r')
+line_1 = in_file.readline()
+line_2 = in_file.readline()
+in_file.close()
+result = int(line_1) + int(line_2.strip())
+print(result)
+
