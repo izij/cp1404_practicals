38bcee2f36c12506da8d87dc3a64bef354e54b01
izij <isabel.king@my.jcu.edu.au>
2022-10-09 14:49:57 +1000

    Complete questions 6-8

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -9,12 +9,21 @@ def main():
         income = float(input(f"Enter income for month {str(month)}: "))
         incomes.append(income)
 
+    print_report(incomes, number_of_months)
+
+
+def print_report(incomes, number_of_months):
     print("\nIncome Report\n-------------")
     total = 0
     for month in range(1, number_of_months + 1):
         income = incomes[month - 1]
-        total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        total = calculate_total(income, total)
+        print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}")
+
+
+def calculate_total(income, total):
+    total += income
+    return total
 
 
 main()
