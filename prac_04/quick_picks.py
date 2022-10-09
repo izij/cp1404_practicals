f62ec884680bdb7b3af5aa432201c82531ac58b7
izij <isabel.king@my.jcu.edu.au>
2022-10-09 21:10:43 +1000

    Add error checking to number_picks

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -5,6 +5,9 @@ NUMBER_MAXIMUM = 45
 NUMBER_OF_NUMBERS = 6
 
 number_picks = int(input("How many quick picks? "))
+while number_picks < 0:
+    print("How can you have negative picks?")
+    number_picks = int(input("How many quick picks? "))
 
 for i in range(number_picks):
     quick_pick = []
