a497ca0a894a54ffc30c449e2903adf91e7e9a15
izij <isabel.king@my.jcu.edu.au>
2022-10-09 21:09:03 +1000

    Check solutions and fix print

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -3,7 +3,6 @@ import random
 NUMBER_MINIMUM = 1
 NUMBER_MAXIMUM = 45
 NUMBER_OF_NUMBERS = 6
-quick_picks = []
 
 number_picks = int(input("How many quick picks? "))
 
@@ -14,10 +13,6 @@ for i in range(number_picks):
         while number in quick_pick:
             number = random.randint(NUMBER_MINIMUM, NUMBER_MAXIMUM)
         quick_pick.append(number)
-        quick_pick.sort()
-    quick_picks.append(quick_pick)
+    quick_pick.sort()
+    print(" ".join(f"{number:2}" for number in quick_pick))
 
-for quick_pick in quick_picks:
-    for number in range(NUMBER_OF_NUMBERS):
-        print(f"{quick_pick[number]:2}", end=" ")
-    print("")
