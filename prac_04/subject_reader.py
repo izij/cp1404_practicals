c9fcd213f7f8d61667b628d770ab585e67fa04e4
izij <isabel.king@my.jcu.edu.au>
2022-10-09 15:04:42 +1000

    Complete tasks

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -3,21 +3,20 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    for line in data:
+        print(f"{line[0]} is taught by {line[1]:12} and has {line[2]:3} students")
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    data_lists = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        data_lists.append(parts)
+    return data_lists
     input_file.close()
 
 
