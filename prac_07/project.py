fad4770426669e2c57909cee897d3546e9df764b
izij <isabel.king@my.jcu.edu.au>
2022-11-03 15:43:14 +1000

    Add __str__ function

--- prac_07/project.py
+++ prac_07/project.py
@@ -11,3 +11,6 @@ class Project:
         self.priority = priority
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
+
+    def __str__(self):
+        return f"{self.name}\t{self.start_date}\t{self.priority}\t{self.cost_estimate}\t{self.completion_percentage}"
