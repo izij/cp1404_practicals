d684cab83d5220c7e0c586dd9b6389a1ec38826b
izij <isabel.king@my.jcu.edu.au>
2022-11-13 14:55:35 +1000

    Add handle_calculation

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -4,6 +4,8 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
 
+CONVERSION_RATE = 1.60934
+
 
 class ConvertMilesApp(App):
     """ ConvertMilesApp is a Kivy App for converting miles into km"""
@@ -14,15 +16,16 @@ class ConvertMilesApp(App):
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
-    def handle_calculation(self):
+    def handle_calculation(self, value):
         """Converts input value into Km"""
+        result = float(value) * CONVERSION_RATE
+        self.root.ids.output_label.text = str(result)
         pass
 
-    def handle_increment(self, value):
+    def handle_increment(self, input_value, change):
         """Adds on value to miles value"""
-        input_value = float(self.root.ids.input_number.text)
-        input_value += value
-        self.root.ids.input_number.text = str(input_value)
+        result = float(input_value) + change
+        self.root.ids.input_number.text = str(result)
 
 
 ConvertMilesApp().run()
