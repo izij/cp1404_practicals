93492f0703d4dba21296811f31f6565f6d340468
izij <isabel.king@my.jcu.edu.au>
2022-11-14 15:27:55 +1000

    Complete stage 2 - Handle invalid inputs

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -22,10 +22,17 @@ class ConvertMilesApp(App):
 
     def handle_calculation(self, value):
         """Converts input value into Km"""
-        self.result_message = str(float(value) * CONVERSION_RATE)
+        try:
+            self.result_message = str(float(value) * CONVERSION_RATE)
+        except ValueError:
+            pass
 
-    def handle_increment(self, input_value, change):
+    def handle_increment(self, text_input, change):
         """Adds on value to miles value"""
+        try:
+            input_value = float(text_input)
+        except ValueError:
+            input_value = 0
         result = float(input_value) + change
         self.root.ids.input_number.text = str(result)
 
