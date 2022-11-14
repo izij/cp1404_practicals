3fec8aa6039cbc2c681757d923031522d061f549
izij <isabel.king@my.jcu.edu.au>
2022-11-14 15:21:24 +1000

    Add StringProperty

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -3,24 +3,26 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
+from kivy.properties import StringProperty
 
 CONVERSION_RATE = 1.60934
 
 
 class ConvertMilesApp(App):
     """ ConvertMilesApp is a Kivy App for converting miles into km"""
+    result_message = StringProperty()
+
     def build(self):
         """ build the Kivy app from the kv file """
         Window.size = (400, 200)
         self.title = "Convert Miles to Kilometers"
         self.root = Builder.load_file('convert_miles_km.kv')
+        self.result_message = "Answer in Km"
         return self.root
 
     def handle_calculation(self, value):
         """Converts input value into Km"""
-        result = float(value) * CONVERSION_RATE
-        self.root.ids.output_label.text = str(result)
-        pass
+        self.result_message = str(float(value) * CONVERSION_RATE)
 
     def handle_increment(self, input_value, change):
         """Adds on value to miles value"""
