d70aa1c6373cfd2981f718e936c388fd0b465ef8
izij <isabel.king@my.jcu.edu.au>
2022-11-19 15:21:19 +1000

    Complete Test Taxi

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,18 @@
+"""
+CP1404 Practical 9
+Test code for Taxi class
+"""
+
+from prac_09.taxi import Taxi
+
+
+def run_tests():
+    my_taxi = Taxi(100, "Prius 1", 1.23)
+    my_taxi.drive(40)
+    print(my_taxi, "total fare is: $", my_taxi.get_fare())
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    print(my_taxi, "total fare is: $", my_taxi.get_fare())
+
+
+run_tests()
