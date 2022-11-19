233d8b3a4e709c1c208af2d8913e21ea2affabd3
izij <isabel.king@my.jcu.edu.au>
2022-11-19 15:24:16 +1000

    Complete Class Variables

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -7,7 +7,7 @@ from prac_09.taxi import Taxi
 
 
 def run_tests():
-    my_taxi = Taxi(100, "Prius 1", 1.23)
+    my_taxi = Taxi(100, "Prius 1")
     my_taxi.drive(40)
     print(my_taxi, "total fare is: $", my_taxi.get_fare())
     my_taxi.start_fare()
