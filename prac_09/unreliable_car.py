4a148b40ae9b444de2e6c2b619cc55ec2bcf0a8d
izij <isabel.king@my.jcu.edu.au>
2022-11-20 10:48:13 +1000

    Add method doc strings

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -12,6 +12,7 @@ class UnreliableCar(Car):
     """Specialised version of Car that includes reliability"""
 
     def __init__(self, fuel, name, reliability=0):
+        """Initialise UnreliableCar class based on Car parent class"""
         super().__init__(fuel, name)
         self.reliability = reliability
 
@@ -20,6 +21,7 @@ class UnreliableCar(Car):
         return f"{super().__str__()}, reliability of {self.reliability}"
 
     def drive(self, distance):
+        """Drive car as in Care parent class, but only if random number generated is less than reliability"""
         if randint(0, 100) < self.reliability:
             return super().drive(distance)
         else:
