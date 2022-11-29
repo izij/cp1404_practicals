c39a216ae9bd55f4868fd17af3a5787546610746
izij <isabel.king@my.jcu.edu.au>
2022-11-30 09:11:57 +1000

    Complete Wiki

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -8,7 +8,10 @@ import wikipedia
 user_input = input("Title or search page: ").title()
 while user_input != "":
     try:
+        wiki_page = wikipedia.page(user_input, auto_suggest=False)
+        print(wiki_page.title)
         print(wikipedia.summary(user_input))
+        print(wiki_page.url)
     except wikipedia.exceptions.DisambiguationError as e:
         print(e.options)
     user_input = input("Title or search page: ").title()
